35551e1075ab1f1cc84c29e4c03da12cace52309
wencakisa <wencakisa@gmail.com>
2017-03-25 15:12:50 +0200

    Reverted back IsNotBlacklisted permission

--- stories/permissions.py
+++ stories/permissions.py
@@ -3,4 +3,9 @@ from rest_framework import permissions
 
 class IsAuthor(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        return obj.author.user == request.user
+        return request.user == obj.author.user
+
+
+class IsNotBlacklisted(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return request.user not in obj.blacklist.all()
