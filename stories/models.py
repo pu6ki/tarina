944a8fd992cc4a65db526f092df53f52bd27e89e
wencakisa <wencakisa@gmail.com>
2017-03-25 12:23:58 +0200

    Initialized StoryLine model

--- stories/models.py
+++ stories/models.py
@@ -14,3 +14,16 @@ class Story(models.Model):
 
     def __str__(self):
         return '{} by {} ({})'.format(self.title, self.author, self.posted_on)
+
+
+class StoryLine(models.Model):
+    story = models.ForeignKey(Story, on_delete=models.CASCADE)
+    content = models.CharField(max_length=250)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    posted_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-posted_on']
+
+    def __str__(self):
+        return 'Story line #{} - {}'.format(self.id, self.story)
