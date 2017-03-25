e14f6dfed821b9cc19d4260fdf144e8d057794d4
wencakisa <wencakisa@gmail.com>
2017-03-25 12:24:45 +0200

    Fixed image extension

--- users/models.py
+++ users/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Author(models.Model):
     user = models.OneToOneField(User)
     profile_image = models.URLField(
-        default='http://tarina.herokuapp.com/static/images/default.png',
+        default='http://tarina.herokuapp.com/static/images/default.jpg',
         blank=False
     )
 
