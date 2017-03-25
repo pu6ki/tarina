9678fa23ac06a0b14aa702bcad6bc339fc0cc2e1
wencakisa <wencakisa@gmail.com>
2017-03-25 11:31:34 +0200

    Fixed wrong path

--- tarina/settings.py
+++ tarina/settings.py
@@ -150,7 +150,7 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
