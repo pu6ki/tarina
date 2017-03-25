d47e3b7216effab8aa067d0a214b071ca77393fd
wencakisa <wencakisa@gmail.com>
2017-03-25 13:41:46 +0200

    Set stories_set to read only field

--- stories/serializers.py
+++ stories/serializers.py
@@ -13,7 +13,7 @@ class StoryLineSerializer(serializers.ModelSerializer):
 class StorySerializer(serializers.ModelSerializer):
     title = serializers.CharField(min_length=3, max_length=100)
     author = AuthorSerializer(read_only=True)
-    storyline_set = StoryLineSerializer(many=True)
+    storyline_set = StoryLineSerializer(read_only=True, many=True)
 
     class Meta:
         model = Story
