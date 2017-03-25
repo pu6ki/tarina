31827a5bfbae872ae78e9d53bfe6b4275abfa7fb
wencakisa <wencakisa@gmail.com>
2017-03-25 11:41:35 +0200

    Added story detail and creation

--- stories/serializers.py
+++ stories/serializers.py
@@ -5,9 +5,15 @@ from .models import Story
 
 
 class StorySerializer(serializers.ModelSerializer):
-    title = serializers.CharField(min_length=3, max_length=3)
+    title = serializers.CharField(min_length=3, max_length=100)
     author = AuthorSerializer(read_only=True)
 
     class Meta:
         model = Story
         fields = ('id', 'title', 'author', 'posted_on')
+
+    def create(self, validated_data):
+        request = self.context['request']
+        author = request.user.author
+
+        return Story.objects.create(author=author, **validated_data)
--- stories/views.py
+++ stories/views.py
@@ -1,7 +1,7 @@
-# from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404
 
-from rest_framework import viewsets
-# from rest_framework.response import Response
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
@@ -14,3 +14,32 @@ class StoriesViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAuthenticated,)
     serializer_class = StorySerializer
     queryset = Story.objects.all()
+
+    def retrieve(self, request, pk=None):
+        story = get_object_or_404(Story, id=pk)
+        self.check_object_permissions(request, story)
+
+        serializer = self.serializer_class(story)
+
+        headers = self.get_success_headers(serializer)
+
+        return Response(
+            serializer.data,
+            status=status.HTTP_200_OK,
+            headers=headers
+        )
+
+    def create(self, request, *args, **kwargs):
+        context = {'request': request}
+
+        serializer = self.serializer_class(data=request.data, context=context)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+
+        headers = self.get_success_headers(serializer)
+
+        return Response(
+            serializer.data,
+            status=status.HTTP_201_CREATED,
+            headers=headers
+        )
