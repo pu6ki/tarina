e54395dfe082156ccb30c31a4841e917e809c743
wencakisa <wencakisa@gmail.com>
2017-03-25 10:11:12 +0200

    Added user login

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 
-from .views import UserRegistration
+from .views import UserRegistration, UserLogin
 
 
 urlpatterns = [
     url(r'^register/', UserRegistration.as_view(), name='register'),
+    url(r'^login/', UserLogin.as_view(), name='login')
 ]
