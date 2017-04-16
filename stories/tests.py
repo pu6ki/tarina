83ec22eee765ec6a68a534fdc6aa016ec6468270
wencakisa <wencakisa@gmail.com>
2017-04-16 15:42:40 +0300

    Added category story list test cases

--- stories/tests.py
+++ stories/tests.py
@@ -9,6 +9,13 @@ from users.models import Author
 from .models import Story, StoryLine
 
 
+HTTP_MESSAGES = {
+    401: 'Authentication credentials were not provided.',
+    403: 'You do not have permission to perform this action.',
+    404: 'Not found.'
+}
+
+
 class StoriesViewSetTests(APITestCase):
     def setUp(self):
         self.list_view_name = 'stories:story-list'
@@ -21,17 +28,14 @@ class StoriesViewSetTests(APITestCase):
             last_name='Lmr'
         )
         self.author = Author.objects.create(user=self.user)
-        self.story = Story.objects.create(title='Test!',author=self.author)
+        self.story = Story.objects.create(title='Test!', author=self.author)
 
         self.request_body = {'title': 'Drama goes down.'}
-        self.http_401_message = 'Authentication credentials were not provided.'
-        self.http_403_message = 'You do not have permission to perform this action.'
-        self.http_404_message = 'Not found.'
 
     def test_story_list_with_unauthorized_user(self):
         response = self.client.get(reverse(self.list_view_name))
 
-        self.assertEqual(response.data['detail'], self.http_401_message)
+        self.assertEqual(response.data['detail'], HTTP_MESSAGES[response.status_code])
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_story_list_with_authorized_user(self):
@@ -45,7 +49,7 @@ class StoriesViewSetTests(APITestCase):
     def test_story_detail_with_unauthorized_user(self):
         response = self.client.get(reverse(self.detail_view_name, kwargs={'pk': self.story.id}))
 
-        self.assertEqual(response.data['detail'], self.http_401_message)
+        self.assertEqual(response.data['detail'], HTTP_MESSAGES[response.status_code])
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_story_detail_with_authorized_user(self):
@@ -61,13 +65,13 @@ class StoriesViewSetTests(APITestCase):
         
         response = self.client.get(reverse(self.detail_view_name, kwargs={'pk': self.story.id + 1}))
 
-        self.assertEqual(response.data['detail'], self.http_404_message)
+        self.assertEqual(response.data['detail'], HTTP_MESSAGES[response.status_code])
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
     def test_story_creation_with_unauthorized_user(self):
         response = self.client.post(reverse(self.list_view_name), self.request_body, format='json')
 
-        self.assertEqual(response.data['detail'], self.http_401_message)
+        self.assertEqual(response.data['detail'], HTTP_MESSAGES[response.status_code])
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_story_creation_with_authorized_user(self):
@@ -80,7 +84,7 @@ class StoriesViewSetTests(APITestCase):
     def test_story_deletion_with_unauthorized_user(self):
         response = self.client.delete(reverse(self.detail_view_name, kwargs={'pk': self.story.id}))
 
-        self.assertEqual(response.data['detail'], self.http_401_message)
+        self.assertEqual(response.data['detail'], HTTP_MESSAGES[response.status_code])
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_story_deletion_of_another_user(self):
@@ -92,7 +96,7 @@ class StoriesViewSetTests(APITestCase):
 
         response = self.client.delete(reverse(self.detail_view_name, kwargs={'pk': self.story.id}))
 
-        self.assertEqual(response.data['detail'], self.http_403_message)
+        self.assertEqual(response.data['detail'], HTTP_MESSAGES[response.status_code])
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_story_deletion_with_valid_user(self):
@@ -101,4 +105,54 @@ class StoriesViewSetTests(APITestCase):
         response = self.client.delete(reverse(self.detail_view_name, kwargs={'pk': self.story.id}))
 
         self.assertEqual(response.data['message'], 'Story successfully deleted.')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+class CategoryStoryListViewTests(APITestCase):
+    def setUp(self):
+        self.view_name = 'stories:category-list'
+
+        self.user = User.objects.create(
+            username='Litelight',
+            password='hasanypairnowrunnin',
+            first_name='Kndrck',
+            last_name='Lmr'
+        )
+        self.author = Author.objects.create(user=self.user)
+        self.story1 = Story.objects.create(title='Test!', author=self.author)
+        self.story2 = Story.objects.create(title='Feel the buzz!', author=self.author)
+
+    def test_category_list_with_unauthorized_user(self):
+        response = self.client.get(reverse(self.view_name, kwargs={'category': 'test'}))
+
+        self.assertEqual(response.data['detail'], HTTP_MESSAGES[response.status_code])
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_category_list_with_invalid_category(self):
+        self.client.force_authenticate(user=self.user)
+
+        response = self.client.get(reverse(self.view_name, kwargs={'category': 'test'}))
+
+        self.assertEqual(response.data['message'], 'Invalid category.')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_personal_category_list(self):
+        self.client.force_authenticate(user=self.user)
+
+        response = self.client.get(reverse(self.view_name, kwargs={'category': 'personal'}))
+
+        self.assertEqual(response.data[0]['title'], self.story2.title)
+        self.assertEqual(response.data[1]['title'], self.story1.title)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_trending_category_list(self):
+        self.client.force_authenticate(user=self.user)
+
+        self.story1.num_vote_up += 3
+        self.story2.num_vote_up += 1
+        self.story1.save()
+        self.story2.save()
+        response = self.client.get(reverse(self.view_name, kwargs={'category': 'trending'}))
+
+        self.assertEqual(response.data[0]['title'], self.story1.title)
+        self.assertEqual(response.data[1]['title'], self.story2.title)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
